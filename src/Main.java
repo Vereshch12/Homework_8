3a53c1d4f830a6d105cf9c7af02d0f4265522d9c
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-26 21:32:03 +0300

    Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+    public static void leapYear(int year){//проверка на високосный год с выдачей результата
+        if (year % 4 == 0) {
+            System.out.println("\n" + year + " год високосный!");
+        }else
+            System.out.println("\n" + year + " год не високосный!");
+
+    }
+
+    public static void task1(){// задание №1
+        int year = 2020;
+        leapYear(year);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
 }
